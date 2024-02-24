b7c9d76054679f420f2c59c0951e7366e49ada51
Praveena Krishnan <praveenakrishnan2003@gmail.com>
2024-02-25 01:09:41 +0530

    first_commit

---
+++ src/reducers/posts.js
@@ -0,0 +1,27 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+export default (posts = [], action) => {
+  switch (action.type) {
+    
+    case DELETE:
+      return posts.filter((post) => (post._id !== action.payload));
+    
+    case UPDATE:
+      return posts.map((post) => (post._id === action.paylod._id ? action.payload : post));
+    
+    case FETCH_ALL:
+      return action.payload;
+    
+    case CREATE:
+      console.log(action.payload)
+      return [...posts,action.pyload];
+    
+      case LIKE:
+        return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+
+    
+    
+    default:
+    
+      return posts;
+  }
+}
